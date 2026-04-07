2c1f630d9dea7ae92e58be80b064d540410378f2
Prerna Thakur <prernathakur9098@gmail.com>
2026-04-07 22:22:55 +0530

    fix allowed hosts error

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=skj!wm$au0=#bgwomhrx86giq(&+opfi4*tv&rn^dcd=jjcka
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
